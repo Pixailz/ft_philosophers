c17dd5b7fce5db9c752015eeb47fe5c906eb681f
Pixailz <Pixailz1@gmail.com>
2022-08-07 17:02:30 +0200

    :x: philo/utils/ft_isnum.c: - fix strict condition

--- philo/utils/ft_isnum.c
+++ philo/utils/ft_isnum.c
@@ -6,7 +6,7 @@
 /*   By: brda-sil <brda-sil@students.42angouleme    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/02 18:24:48 by brda-sil          #+#    #+#             */
-/*   Updated: 2022/08/02 23:12:28 by brda-sil         ###   ########.fr       */
+/*   Updated: 2022/08/07 16:56:57 by brda-sil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int	ft_isnumeric(const char *str)
 	if (*ptr_str == '-' || *ptr_str == '+')
 		ptr_str++;
 	while (*ptr_str++)
-		if (*(ptr_str - 1) <= '0' || *(ptr_str - 1) >= '9')
+		if (*(ptr_str - 1) < '0' || *(ptr_str - 1) > '9')
 			return (0);
 	return (1);
 }
