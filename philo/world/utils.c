d0e3278e99f0fea983040fd4d9a1346052167a12
Pixailz <Pixailz1@gmail.com>
2022-08-08 19:32:15 +0200

    :fire: philo/world/utils.c: - add check for have_max_eat (more efficient)

--- philo/world/utils.c
+++ philo/world/utils.c
@@ -6,7 +6,7 @@
 /*   By: brda-sil <brda-sil@students.42angouleme    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/08 01:19:51 by brda-sil          #+#    #+#             */
-/*   Updated: 2022/08/08 01:20:10 by brda-sil         ###   ########.fr       */
+/*   Updated: 2022/08/08 19:19:05 by brda-sil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,14 @@
 
 void	ft_lock_both(t_main *config)
 {
-	pthread_mutex_lock(&config->m_all_ate);
 	pthread_mutex_lock(&config->m_have_died);
+	if (config->have_max_eat)
+		pthread_mutex_lock(&config->m_all_ate);
 }
 
 void	ft_unlock_both(t_main *config)
 {
-	pthread_mutex_unlock(&config->m_all_ate);
 	pthread_mutex_unlock(&config->m_have_died);
+	if (config->have_max_eat)
+		pthread_mutex_unlock(&config->m_all_ate);
 }
