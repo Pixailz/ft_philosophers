0e1af5525073db70de2874123c0edbf674d3ff9e
Bruno Da silva <brda-sil@1E4.42angouleme.fr>
2022-08-14 03:05:14 +0200

    :package: philo_bonus/dataset/free.c - improve readability

--- philo_bonus/dataset/free.c
+++ philo_bonus/dataset/free.c
@@ -6,7 +6,7 @@
 /*   By: brda-sil <brda-sil@students.42angouleme    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/07 21:44:30 by brda-sil          #+#    #+#             */
-/*   Updated: 2022/08/12 13:22:32 by brda-sil         ###   ########.fr       */
+/*   Updated: 2022/08/14 02:25:14 by brda-sil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,14 +24,27 @@ void	free_philos(t_main *config)
 	free(config->philo_status);
 }
 
+void	unlink_semaphore(void)
+{
+	sem_unlink("/s_forks");
+	sem_unlink("/s_speak");
+	sem_unlink("/s_begin");
+	sem_unlink("/s_last_meal");
+	sem_unlink("/s_nb_eat");
+	sem_unlink("/s_take_fork");
+	sem_unlink("/s_have_died");
+}
+
 void	destroy_semaphore(t_main *config)
 {
 	sem_close(config->s_forks);
 	sem_close(config->s_speak);
 	sem_close(config->s_begin);
-	sem_unlink("/s_forks");
-	sem_unlink("/s_speak");
-	sem_unlink("/s_begin");
+	sem_close(config->s_last_meal);
+	sem_close(config->s_nb_eat);
+	sem_close(config->s_take_fork);
+	sem_close(config->s_have_died);
+	unlink_semaphore();
 }
 
 void	free_entry(t_main *config)
