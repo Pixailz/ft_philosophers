676a6d1a30b28d898648f89d4f9c9596483ea7af
Bruno Da silva <brda-sil@1E4.42angouleme.fr>
2022-08-18 22:02:09 +0200

    :mute: philo_bonus/god_bonus.c - remove commentary

--- philo_bonus/world/god_bonus.c
+++ philo_bonus/world/god_bonus.c
@@ -6,7 +6,7 @@
 /*   By: brda-sil <brda-sil@students.42angouleme    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/12 03:13:43 by brda-sil          #+#    #+#             */
-/*   Updated: 2022/08/14 14:52:30 by brda-sil         ###   ########.fr       */
+/*   Updated: 2022/08/17 21:37:43 by brda-sil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,6 @@ void	kill_all(t_main *config)
 	int	counter;
 
 	counter = 0;
-	// say(config->philos[philo_id], "has died");
-	//sem_wait(config->s_speak);
 	while (counter < config->number_of_philosophers)
 		kill(config->philo_pid_table[counter++], 9);
 	if (DEBUG)
@@ -50,7 +48,6 @@ int	check_pid_status(t_main *config)
 		{
 			waitpid(config->philo_pid_table[counter], \
 									&config->philo_status[counter], WNOHANG);
-			// printf("philo[%d] has exited\n", counter + 1);
 			if (WIFEXITED(config->philo_status[counter]))
 			{
 				if (config->philo_status[counter] >> 8 == 1)
