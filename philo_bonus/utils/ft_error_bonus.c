0fb2e808f6b0dcecbd01b24234a34b87e12dc068
Bruno Da silva <brda-sil@1E4.42angouleme.fr>
2022-08-18 21:58:21 +0200

    :package: ft_error - redirect write to stderr

--- philo_bonus/utils/ft_error_bonus.c
+++ philo_bonus/utils/ft_error_bonus.c
@@ -1,83 +1,84 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_error.c                                         :+:      :+:    :+:   */
+/*   ft_error_bonus.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: brda-sil <brda-sil@students.42angouleme    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/02 18:14:39 by brda-sil          #+#    #+#             */
-/*   Updated: 2022/08/14 03:12:00 by brda-sil         ###   ########.fr       */
+/*   Updated: 2022/08/17 00:15:39 by brda-sil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers_bonus.h"
 
+int	ft_putstr_strderr(const char *msg)
+{
+	char	*msg_ptr;
+
+	msg_ptr = (char *)msg;
+	while (*msg_ptr)
+		msg_ptr++;
+	return (write(2, msg, msg_ptr - msg));
+}
+
 int	error_parse(int ret_code)
 {
-	printf("Error: ");
+	ft_putstr_strderr("Error: ");
 	if (ret_code == 1 || ret_code == 6 || ret_code == 11 || ret_code == 16)
-		printf("(number of philosophers)\n");
+		ft_putstr_strderr("(number of philosophers)\n");
 	if (ret_code == 2 || ret_code == 7 || ret_code == 12 || ret_code == 17)
-		printf("(time to die)\n");
+		ft_putstr_strderr("(time to die)\n");
 	if (ret_code == 3 || ret_code == 8 || ret_code == 13 || ret_code == 18)
-		printf("(time to eat)\n");
+		ft_putstr_strderr("(time to eat)\n");
 	if (ret_code == 4 || ret_code == 9 || ret_code == 14 || ret_code == 19)
-		printf("(time to sleep)\n");
+		ft_putstr_strderr("(time to sleep)\n");
 	if (ret_code == 5 || ret_code == 10 || ret_code == 15 || ret_code == 20)
-		printf("(max eat)\n");
+		ft_putstr_strderr("(max eat)\n");
 	if (ret_code <= 5)
-		printf("String cannot be empty\n");
+		ft_putstr_strderr("String cannot be empty\n");
 	if (ret_code > 5 && ret_code <= 10)
-		printf("String is not numeric\n");
+		ft_putstr_strderr("String is not numeric\n");
 	if (ret_code > 10 && ret_code <= 15)
-		printf("String can't be negative\n");
+		ft_putstr_strderr("String can't be negative\n");
 	if (ret_code > 15 && ret_code <= 20)
-		printf("String is too big\n");
+		ft_putstr_strderr("String is too big\n");
 	return (ret_code);
 }
 
 int	error_init(int return_code)
 {
-	printf("Error: ");
+	ft_putstr_strderr("Error: ");
 	if (return_code == 1)
-		printf("malloc (*philo_pid_table)\n");
+		ft_putstr_strderr("malloc (*philo_pid_table)\n");
 	if (return_code == 2)
-		printf("malloc (*philo_status)\n");
+		ft_putstr_strderr("malloc (*philo_status)\n");
 	if (return_code == 3)
-		printf("malloc (**philos)\n");
+		ft_putstr_strderr("malloc (**philos)\n");
 	if (return_code == 4)
-		printf("malloc (*philo)\n");
+		ft_putstr_strderr("malloc (*philo)\n");
 	if (return_code == 5)
-		printf("init semaphore (s_forks)\n");
+		ft_putstr_strderr("init semaphore (s_forks)\n");
 	if (return_code == 6)
-		printf("init semaphore (s_speak)\n");
+		ft_putstr_strderr("init semaphore (s_speak)\n");
 	if (return_code == 7)
-		printf("init semaphore (s_begin)\n");
+		ft_putstr_strderr("init semaphore (s_begin)\n");
 	if (return_code == 8)
-		printf("init semaphore (s_last_meal)\n");
+		ft_putstr_strderr("init semaphore (s_last_meal)\n");
 	if (return_code == 9)
-		printf("init semaphore (s_nb_eat)\n");
+		ft_putstr_strderr("init semaphore (s_nb_eat)\n");
 	if (return_code == 10)
-		printf("init semaphore (s_take_fork)\n");
+		ft_putstr_strderr("init semaphore (s_take_fork)\n");
 	if (return_code == 11)
-		printf("init semaphore (s_have_died)\n");
+		ft_putstr_strderr("init semaphore (s_have_died)\n");
 	return (return_code);
 }
 
 int	error_life(int return_code)
 {
 	if (return_code == 1)
-		printf("Error: (create thread)");
+		ft_putstr_strderr("Error: (create thread)");
 	if (return_code == 2)
-		printf("Error: (joining thread)");
+		ft_putstr_strderr("Error: (joining thread)");
 	return (return_code);
-}
-
-int	ft_error(char *msg)
-{
-	if (msg)
-		printf("Error: %s\n", msg);
-	else
-		printf("Error\n");
-	return (-1);
 }
