39ff11d930d82c6f236c30f4b1539699ea534a56
Pixailz <pixailz1@gmail.com>
2022-08-26 18:47:16 +0200

    :non-potable_water: philo/world/say.c - fix data race

--- philo/world/say.c
+++ philo/world/say.c
@@ -6,7 +6,7 @@
 /*   By: brda-sil <brda-sil@students.42angouleme    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/07 19:54:41 by brda-sil          #+#    #+#             */
-/*   Updated: 2022/08/08 01:33:02 by brda-sil         ###   ########.fr       */
+/*   Updated: 2022/08/26 17:47:18 by brda-sil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,10 @@ void	say(t_philo *philo, char *action)
 {
 	t_stamp	current_ts;
 
+	ft_lock_both(philo->config);
 	if (!philo->config->all_ate && !philo->config->have_died)
 	{
+		ft_unlock_both(philo->config);
 		current_ts = ft_get_timestamp_ms() - philo->config->start_ts;
 		pthread_mutex_lock(&philo->config->m_speak);
 		if (DEBUG)
@@ -27,5 +29,7 @@ void	say(t_philo *philo, char *action)
 		printf(" %d ", philo->philo_id);
 		printf("%s\n", action);
 		pthread_mutex_unlock(&philo->config->m_speak);
+		return ;
 	}
+	ft_unlock_both(philo->config);
 }
